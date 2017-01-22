df6b12bc2b20211bd74bac33784f9272b0b14459
Ricardo Bandala <ricardobandala@gmail.com>
2017-01-22 17:40:24 -0500

    appy cart rules on form even on update products

--- wordpress/wp-content/plugins/blushdrop_plugin/customerCartRules.php
+++ wordpress/wp-content/plugins/blushdrop_plugin/customerCartRules.php
@@ -2,6 +2,9 @@
 $onePerCart = $settings['cartRules']['onePerCart']; //=>['prodID_EditingPacakage','prodID_URL'],
 $noModifyQuantity = $settings['cartRules']['noModifyQuantity']; //=>['minutes'],
 ?>
+<div id="bdp_background" class="bdp_background">
+    <div class="bdp-spinner mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active"></div>
+</div>
 <div
     id="bdp_cr"
     data-opc="<?= json_encode($onePerCart)?>"
